503ca824632823c711e7cdc7423838c907b9e290
Cristian Miranda <crism60@gmail.com>
2017-08-15 18:34:04 -0300

    Ejemplo 3

---
+++ Flex&Bison/Linux/Ejemplo3/ejemplo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+flex lexico.l
+read -p "Léxico Generado... Continuar?"
+bison -dyv sintaxis.y
+read -p "Sintáxis Generada... Continuar?"
+gcc -w lex.yy.c y.tab.c -o compilador.bin
+read -p "Compilación finalizada... Continuar?"
+./compilador.bin Prueba.txt
+rm lex.yy.c
+rm y.tab.c
+rm y.output
+rm y.tab.h
+rm compilador.bin
+read -p "Ejemplo finalizado!"
