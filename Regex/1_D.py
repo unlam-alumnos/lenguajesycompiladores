2e774fe3fb96d59f0910e6f8071344c6e7635a77
Cristian Miranda <crism60@gmail.com>
2017-08-16 18:42:31 -0300

    Regex - 1D

---
+++ Regex/1_D.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from utils.RegexUtils import RegexUtils
+
+REGEX = r'^([a-zA-Z]{1}|[a-zA-Z]{1}-*([a-zA-Z0-9]+-*)*[^-])$';
+
+def main():
+    assert RegexUtils.match(REGEX, 'a') == True
+    assert RegexUtils.match(REGEX, 'a-2') == True
+    assert RegexUtils.match(REGEX, 'a-a') == True
+    assert RegexUtils.match(REGEX, 'a-a-2-3-1-3') == True
+    assert RegexUtils.match(REGEX, 'a-a-2-3-------1-3') == True
+    assert RegexUtils.match(REGEX, 'A-') == False
+    assert RegexUtils.match(REGEX, '-131-412') == False
+    assert RegexUtils.match(REGEX, '131-aaa') == False
+    assert RegexUtils.match(REGEX, '-') == False
+    assert RegexUtils.match(REGEX, '') == False
+    assert RegexUtils.match(REGEX, 'this-is-valid----not-') == False
+    print "Ejercicio finalizado con exito!"
+
+if __name__ == "__main__":
+    main()
